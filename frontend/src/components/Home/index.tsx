473fca21a987ecd10cd335944c7fb95546f1591b
Alexandre Krispin <k.m.alexandre@protonmail.com>
2022-06-17 08:56:34 +0900

    Refactor Jumbotron

--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -8,7 +8,7 @@ import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import CssBaseline from "@mui/material/CssBaseline";
-import MoreTimeIcon from "@mui/icons-material/MoreTime";
+import Jumbotron from "./Jumbotron";
 import SquareLocation from "./SquareLocation";
 import { Location } from "types/Location";
 
@@ -40,47 +40,7 @@ const Home = ({ location }: HomeProps) => {
         <Grid item xs={12}>
           {loginButton}
         </Grid>
-        <Grid item xs={12}>
-          <div style={{ position: "relative" }}>
-            <figure style={{ margin: 0 }}>
-              <img
-                width="100%"
-                height="100%"
-                alt="hair_style_woman_picture"
-                src="https://unsplash.com/photos/HEde-a_T4E8/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8NHx8c3R5bGlzdHxlbnwwfHx8fDE2NTQ1Mjc5NzU&force=true&w=2400"
-              />
-              <figcaption className="figureCaption">
-                <Typography variant="caption" display="block" gutterBottom>
-                  Photo by{" "}
-                  <a href="https://unsplash.com/@awcreativeut?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                    Adam Winger
-                  </a>{" "}
-                  on{" "}
-                  <a href="https://unsplash.com/photos/HEde-a_T4E8?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink">
-                    Unsplash
-                  </a>
-                </Typography>
-              </figcaption>
-            </figure>
-              <Card className="businessNameCard">
-                <CardContent className="cardContent">
-                  <Typography color="white" variant="h4">
-                    {location.business_name}
-                  </Typography>
-                  <Link to="book" style={{ textDecoration: "none" }}>
-                    <Button
-                      className="businessNameButton"
-                      variant="contained"
-                      size="large"
-                      endIcon={<MoreTimeIcon />}
-                    >
-                      Book Now
-                    </Button>
-                  </Link>
-                </CardContent>
-              </Card>
-          </div>
-        </Grid>
+        <Jumbotron location={location} />
         <SquareLocation location={location} />
       </Grid>
     </CssBaseline>
