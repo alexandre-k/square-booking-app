f41264bf3a33d0ba88aa8d3b01415299979f9fcf
Alexandre Krispin <k.m.alexandre@protonmail.com>
2022-06-17 09:33:13 +0900

    Refactor login

--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -1,48 +1,41 @@
 import * as React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 import "./index.css";
-import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
+import AppBar from "@mui/material/AppBar";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import CssBaseline from "@mui/material/CssBaseline";
 import Jumbotron from "./Jumbotron";
+import Login from "./Login";
 import SquareLocation from "./SquareLocation";
 import { Location } from "types/Location";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 interface HomeProps {
   location: Location;
 }
 
-const Home = ({ location }: HomeProps) => {
-  const { isAuthenticated } = useAuth0();
+console.log("TODO: Create custom theme color");
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#00A0E5",
+    },
+  },
+});
 
-  console.log("TODO: Properly handle undefined location");
-  const loginButton = isAuthenticated ? (
-    <Link to="logout">
-      <Button>Logout</Button>
-    </Link>
-  ) : (
-    <Link to="login">
-      <Button>Login</Button>
-    </Link>
-  );
+const Home = ({ location }: HomeProps) => {
   return (
     <CssBaseline enableColorScheme>
-      <Grid
-        container
-        spacing={2}
-        alignItems="center"
-        justifyContent="space-around"
-      >
-        <Grid item xs={12}>
-          {loginButton}
+        {/* <ThemeProvider theme={theme}> */}
+        <Grid container alignItems="center" justifyContent="space-around">
+          <Grid item xs={12}>
+            <AppBar position="static">
+              <Login />
+            </AppBar>
+          </Grid>
+          <Jumbotron location={location} />
+          <SquareLocation location={location} />
         </Grid>
-        <Jumbotron location={location} />
-        <SquareLocation location={location} />
-      </Grid>
+        {/* </ThemeProvider> */}
     </CssBaseline>
   );
 };
