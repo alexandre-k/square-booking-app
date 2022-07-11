3a14953d4d12088b69b61796bee333175489d70c
Alexandre Krispin <k.m.alexandre@protonmail.com>
2022-07-11 21:45:02 +0900

    Add echo

--- infra/scripts/redeploy.sh
+++ infra/scripts/redeploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo "cd to backend"
 cd /home/guest/square-booking-app/backend
+echo "backend: git pull"
 git pull
+echo "cd frontend"
 cd ..
+echo "frontend: git pull"
 git pull
+echo "Change run_app.sh as executable"
 chmod +x ./infra/scripts/run_app.sh
+echo "Run run_app.sh"
 ./infra/scripts/run_app.sh
