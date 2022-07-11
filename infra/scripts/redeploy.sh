1b774fadc579f52f7f59cf6d66bd8ff750d2576f
Alexandre Krispin <k.m.alexandre@protonmail.com>
2022-07-11 22:50:32 +0900

    Fix permission

--- infra/scripts/redeploy.sh
+++ infra/scripts/redeploy.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
+su -c 
 id
-whoami
-git config --global --add safe.directory /home/guest/square-booking-app/backend
-echo "Cd to backend"
-cd /home/guest/square-booking-app/backend
 echo "backend: git pull"
-git pull
-echo "cd frontend"
-cd ..
+su -c "cd /home/guest/square-booking-app/backend; git pull" -m guest
 
-git config --global --add safe.directory /home/guest/square-booking-app
 echo "frontend: git pull"
-git pull
-echo "Change run_app.sh as executable"
-chmod +x ./infra/scripts/run_app.sh
+su -c "cd /home/guest/square-booking-app; git pull" -m guest
+
 echo "Run run_app.sh"
-./infra/scripts/run_app.sh
+su -c "chmod +x /home/guest/square-booking-app/infra/scripts/run_app.sh" -m guest
+su -c "/bin/bash /home/guest/square-booking-app/infra/scripts/run_app.sh" -m guest
